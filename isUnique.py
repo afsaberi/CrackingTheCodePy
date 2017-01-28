9b9f35d399659c04b8ceef928cefaa268d2c4fee
Ali <afsaberi23@gmail.com>
2017-01-28 01:18:19 -0500

    Adding is Unique

---
+++ isUnique.py
@@ -0,0 +1,12 @@
+def isUnique(string):
+    string=string
+    list=[]
+    for i in string:
+        if i in list and i !=" ":
+            print "not unique"
+            return False
+        else:
+            list.append(i)
+    print "Is unique"
+    return True
+isUnique("hello")
